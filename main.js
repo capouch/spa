392a9ba27973c091c8042a593fba3415870a74f7
Brian Capouch <brianc@proto.palaver.net>
2015-07-12 04:26:46 -0400

    Clean up from playing with IPC functions

--- main.js
+++ main.js
@@ -17,11 +17,13 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
+
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
