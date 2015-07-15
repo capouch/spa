452edede2b9d36ab9f41a7b44ce30efd9a14638a
Brian Capouch <brianc@saintjoe.edu>
2015-07-15 19:19:32 +0000

    Run on non-conflicting port

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ var
 // -------------- END SERVER CONFIGURATION ----------------
 
 // ----------------- BEGIN START SERVER -------------------
-server.listen( 8000 );
+server.listen( 8001 );
 console.log(
   'Express server listening on port %d in %s mode',
    server.address().port, app.settings.env
