8e6a282b9a44c8cfcdc7521fd1708cd459ce3cb5
Brian Capouch <brianc@proto.palaver.net>
2015-07-16 04:51:08 -0400

    Revert to standard port for merged master branch

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ var
 // -------------- END SERVER CONFIGURATION ----------------
 
 // ----------------- BEGIN START SERVER -------------------
-server.listen( 8001 );
+server.listen( 8000 );
 console.log(
   'Express server listening on port %d in %s mode',
    server.address().port, app.settings.env
