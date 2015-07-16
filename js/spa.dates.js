71849b98c1a497775f0faae9df731b662a38c76e
Brian Capouch <brianc@proto.palaver.net>
2015-07-16 04:54:59 -0400

    Remove old dead-ish code

--- js/spa.dates.js
+++ js/spa.dates.js
@@ -257,13 +257,6 @@ spa.dates = (function () {
       //Swap actual section contents
       swapSection();
     }); // End handler for view toggle button
-
-
-    // Test moment library functions by showing my age
-    var now = moment(),
-      startday = moment('1951-02-20');
-    jqueryMap.$container.append('<br>Date now: ' 
-      + now.format("dddd, MMMM Do YYYY"));
  } // End public method /initModule
 
   // Begin method /postSection/
