2e64a71efa85c8b46d3c95bff8c68147df860ed1
Craig <cav9871@saintjoe.edu>
2015-07-17 00:34:18 -0400

    moved some code around

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ var
 // -------------- END SERVER CONFIGURATION ----------------
 
 // ----------------- BEGIN START SERVER -------------------
-server.listen( 8000u );
+server.listen(8000);
 console.log(
   'Express server listening on port %d in %s mode',
    server.address().port, app.settings.env
