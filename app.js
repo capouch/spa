d999e7976afc76d90e29e8be29246f3e65c0a2e1
Nathan Samano <nsv9982@saintjoe.edu>
2015-07-19 17:12:51 -0400

    added print line

--- app.js
+++ app.js
@@ -17,18 +17,47 @@
 // ------------ BEGIN MODULE SCOPE VARIABLES --------------
 'use strict';
 var
+  setWatch,
+
   http    = require( 'http'                 ),
   express = require( 'express'              ),
   bodyParser = require('body-parser'        ),
   methodOverride = require('method-override'),
   morgan = require('morgan'		    ),
+  socketIo = require( 'socket.io'),
+
   app     = express(),
   router = express.Router(),
   routes = require('./js/routes.js'),
   server  = http.createServer( app ),
-  io = require('socket.io').listen(server);
+  io = socketIo.listen(server),
+  watchMap = {}
+  ;
 
 // ------------- END MODULE SCOPE VARIABLES ---------------
+
+// --------------- BEGIN UTILITY METHODS ------------------
+setWatch = function ( url_path, file_type ) {
+  console.log( 'setWatch called on ' + url_path );
+
+  if ( ! watchMap[ url_path ] ) {
+    console.log( 'setting watch on ' + url_path );
+
+    fsHandle.watchFile(
+      url_path.slice(1),
+      function ( current, previous ) {
+        console.log( 'file accessed' );
+        if ( current.mtime !== previous.mtime ) {
+          console.log( 'file changed' );
+          io.sockets.emit( file_type, url_path );
+        }
+      }
+    );
+    watchMap[ url_path ] = true;
+  }
+
+};
+// ---------------- END UTILITY METHODS -------------------
 
 // ------------- BEGIN SERVER CONFIGURATION ---------------
   app.use( express.static( __dirname + '' ) );
@@ -40,16 +69,34 @@ var
   routes.configRoutes( router, server );
   app.use('/', router);
 
+  app.use( function ( request, response, next ) {
+    console.log("in app.use");
+    if ( request.url.indexOf( '/js/'  ) >= 0 ) {
+      setWatch( request.url, 'script' );
+    }
+    else if ( request.url.indexOf( '/css/' ) >= 0 ) {
+      setWatch( request.url, 'stylesheet' );
+    }
+    next();
+
+    app.use( express.static( __dirname + '/' ) );
+  });
+
   // Socket.IO Functions
   app.get('/', function(req, res){
+      console.log("in app.get");
       res.sendFile(__dirname + './html/index.html');
   });
 
   io.on('connection', function(socket){
+    console.log("making io connection");
     socket.on('chat message', function(msg){
       io.emit('chat message', msg);
+      console.log("sending chat message");
     });
   });
+
+  
 
 // -------------- END SERVER CONFIGURATION ----------------
 
