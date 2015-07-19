d245810be9fbcbb1cbb9c565b22714aef9234d05
Brian Capouch <brianc@proto.palaver.net>
2015-07-19 07:29:30 -0400

    Too late to worry about this

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var app = require('app');  // Module to control application life.
+
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 
 // Report crashes to our server.
