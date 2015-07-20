fc96dd5a478e34788c47dd34f1ad90b8851051c4
Brian Capouch <brianc@proto.palaver.net>
2015-07-20 04:44:25 -0400

    Comment out chat code so Electron won't freak out

--- js/spa.socket.js
+++ js/spa.socket.js
@@ -100,21 +100,21 @@ spa.socket = (function () {
     //queryMap.$container.show();
 
     // socket.io
-    socket = io();
-    $('form').submit(function(){
-      socket.emit('chat message', $('#m').val());
-      $('#m').val('');
-      return false;
-    });
-    socket.on('chat message', function(msg){
-      $('#messages').append($('<li>').text(msg));
-      // keep scrollbar at bottom
-      $('#messages').scrollTop(100000000);
-    });
-
+    // socket = io();
+//    $('form').submit(function(){
+//      socket.emit('chat message', $('#m').val());
+//      $('#m').val('');
+//      return false;
+//    });
+//    socket.on('chat message', function(msg){
+//      $('#messages').append($('<li>').text(msg));
+//      // keep scrollbar at bottom
+//      $('#messages').scrollTop(100000000);
+//    });
+//
   };
 
-  postSection = function() {
+    postSection = function() {
     jqueryMap.$socketIO.show();
     jqueryMap.$container.show();
   };
