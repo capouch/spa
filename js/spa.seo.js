b0472186a12f587c17183b1f13a05e63534e928e
Brian Capouch <brianc@proto.palaver.net>
2015-07-24 02:37:19 -0400

    Clean up for show

--- js/spa.seo.js
+++ js/spa.seo.js
@@ -5,58 +5,61 @@
 
 spa.seo = (function () {
   'use strict';
-  //---------------- BEGIN MODULE SCOPE VARIABLES --------------
+  //--- Local variables
   var
     configMap = {
       main_html : String() 
-        + '<section id="seoPage">SEO Demonstration'
+      + '<section id="seoPage">SEO Demonstration'
       + '</section>'
    
     },
+
     stateMap = {
       $container  : undefined
     },
+
     jqueryMap = {},
     initModule, copyAnchorMap, setJqueryMap, setClicks,postSection, seoPage;
     
-  //----------------- END MODULE SCOPE VARIABLES ---------------
+  //--- end local variables
 
-// Begin DOM method /setJqueryMap/
+
+
+  // Begin DOM method /setJqueryMap
   setJqueryMap = function () {
-    var $container = stateMap.$container;
+   var $container = stateMap.$container;
 
-    // Set initial jQuery map values
-    jqueryMap = {
+   // Set initial jQuery map values
+   jqueryMap = {
       $container : $container,
       $seoPage  : $container.find('#seoPage')
     };
   };
-  // End DOM method /setJqueryMap/
+  // End DOM method /setJqueryMap
 
-  //--------------------- BEGIN PUBLIC METHODS --------------------
+  //--- Public methods
   initModule = function ( $container ) {
+
     // load HTML and map jQuery collections
     stateMap.$container = $container;
     $container.hide();
     $container.html( configMap.main_html );
 
     setJqueryMap();
-    //jqueryMap.$socketIO.html( configMap.main_html );
-    //queryMap.$container.show();
-
   };
 
 
   
-    // Begin method /postSection/
+  // Begin method /postSection
   // Normal entry point - Just render container contents
   postSection = function() {
     jqueryMap.$seoPage.show();
     jqueryMap.$container.show();
   } // end postSection
 
+  // We only expose two methods to the outside
   return { initModule : initModule,
           postSection : postSection
          };
-  //------------------- END PUBLIC METHODS ---------------------
+  //--- end public methods
 })();
