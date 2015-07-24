22124f4af1d53f991266b1cef857cad10787fd0d
Brian Capouch <brianc@proto.palaver.net>
2015-07-24 01:31:38 -0400

    Clean up options object

--- js/routes.js
+++ js/routes.js
@@ -2,69 +2,43 @@
  * routes.js - module to provide routing
 */
 
-/*jslint         node    : true, continue : true,
-  devel  : true, indent  : 2,    maxerr   : 50,
-  newcap : true, nomen   : true, plusplus : true,
-  regexp : true, sloppy  : true, vars     : false,
-  white  : true
-*/
-/*global */
-
-// ------------ BEGIN MODULE SCOPE VARIABLES --------------
+// --- Local variables
 'use strict';
 var
   configRoutes;
-// ------------- END MODULE SCOPE VARIABLES ---------------
+// --- End variable declarations
 
-// ---------------- BEGIN PUBLIC METHODS ------------------
+// --- Public API
 
 configRoutes = function ( router, server ) {
-  router.get('/', function(req, res) {
-    var options = {
-      // I don't like this very much 
-      root: __dirname + '/../html/'
+
+  // Serve files from html sibling directory
+  var options = {
+    root: __dirname + '/../html/'
     };
+
+  router.get('/', function(req, res) {
     res.sendFile('index.html', options);
   });
 
   router.get('/dates', function(req, res) {
-    var options = {
-      // I don't like this very much
-      root: __dirname + '/../html/'
-    };
     res.sendFile('index.html', options);
   });
 
   router.get('/socket', function(req, res) {
-    var options = {
-      // I don't like this very much
-      root: __dirname + '/../html/'
-    };
     res.sendFile('index.html', options);
   });
 
   // This will be commented out before the show
   router.get('/seo', function(req, res) {
-    var options = {
-      // I don't like this very much
-      root: __dirname + '/../html/'
-    };
     res.sendFile('index.html', options);
   });
   
   router.get('/robots.txt', function(req, res) {
-    var options = {
-      // I don't like this very much
-      root: __dirname + '/../html/'
-    };
     res.sendFile('robots.txt', options);
   });
   
-    router.get('/Sitemap.xml', function(req, res) {
-    var options = {
-      // I don't like this very much
-      root: __dirname + '/../html/'
-    };
+ router.get('/Sitemap.xml', function(req, res) {
     res.sendFile('Sitemap.xml', options);
   });
 
